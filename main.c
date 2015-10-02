b3ae515345c372b2cf0098b9c7ffcd2d51d4bd42
Alex <alexpolcyn65@gmail.com>
2015-10-02 14:03:52 -0700

    working, added in comments

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <msp430.h> 
 
-int action = 0;
+int action = 0;  // global to keep track of what action the button press should take
 
 void write_cmd(char cmd) {
 	P2OUT = 0;
@@ -8,9 +8,9 @@ void write_cmd(char cmd) {
 	// upper nibble
 	P1OUT = cmd;
 	__delay_cycles(10);
-	P2OUT = BIT0;
+	P2OUT = BIT0;      // raise enable
 	__delay_cycles(10);
-	P2OUT = 0;
+	P2OUT = 0;   // lower enable
 
 	// lower nibble
 	__delay_cycles(10);
@@ -20,8 +20,8 @@ void write_cmd(char cmd) {
 	__delay_cycles(10);
 	P2OUT = 0;
 
-    __delay_cycles(2000000);
-    P1OUT = 0;
+    __delay_cycles(2000000);  // wait a long time, allow operation to complete
+    P1OUT = 0; // clear P1OUT,just keeping don't care lines low
 }
 
 void write_data(char data) {
@@ -32,9 +32,9 @@ void write_data(char data) {
 	// upper nibble
 	P1OUT = data;
 	__delay_cycles(10);
-	P2OUT |= BIT0;
+	P2OUT |= BIT0;     // set enable high
 	__delay_cycles(10);
-	P2OUT &= BIT2;
+	P2OUT &= BIT2;     // lower enable, but keep RS high
 
 	// lower nibble
 	__delay_cycles(10);
@@ -43,10 +43,11 @@ void write_data(char data) {
 	P2OUT |= BIT0;
 	__delay_cycles(10);
 	P2OUT &= BIT2;
-    __delay_cycles(2000000);
-    P1OUT = 0;
+    __delay_cycles(2000000); // Wait a long time, allow operation to complete
+    P1OUT = 0;  // clear P1OUT,just keeping don't care lines low
 }
 
+/* Writes a string of characters to DDRAM */
 write_msg(char* arr) {
 	while(*arr) {
 		write_data(*arr++);
@@ -72,11 +73,12 @@ int main(void) {
     }
     DCOCTL = 0; // Select lowest DCOx and MODx settings
     BCSCTL1 = CALBC1_16MHZ; // Set range
-    DCOCTL = CALDCO_16MHZ;
+    DCOCTL = CALDCO_16MHZ;  // MCLK set to 16MHz
 
     /* initialize */
     __delay_cycles(16000000); // delay 1/4 second after power up
 
+    /* Write the first command to put it in nibble mode */
     P2OUT = 0;
     P1OUT = BIT5;
     __delay_cycles(10);
@@ -98,11 +100,7 @@ int main(void) {
 
     write_cmd(BIT1); // return home, set DDRAM address to 0x00
 
-    write_cmd(BIT5 | BIT3 | BIT2); // function set,
-
     write_msg("hello world!");
-
-    write_cmd(BIT5 | BIT3 | BIT2);
 
     P1IE = 0;
 
@@ -125,28 +123,33 @@ __interrupt void button(void) {
 
 	switch(action) {
 	case 0:
-		write_cmd(BIT1);
+		write_cmd(BIT1);  // return home
 		write_msg("just pressed");
 		write_cmd(BIT1); // return home
 		action = 1;
 		break;
 	case 1:
-		write_cmd(BIT1);
-		write_cmd(BIT7 | 0x40);
+		write_cmd(BIT1);  // return home
+		write_cmd(BIT7 | 0x40);  // shift cursor to lower left corner
 		write_msg("pressed again");
 		write_cmd(BIT1); // return home
 		action = 2;
 		break;
 	case 2:
 		write_cmd(BIT1);
-		for(i = 0; i < 80; i++) {
+		/* Cycle display, shifting to the right */
+		for(i = 0; i < 40; i++) {
 			 write_cmd(BIT4 | BIT3 | BIT2);
+		}
+		/* Cycle display, shifting to the left */
+		for(i = 0; i < 40; i++) {
+			 write_cmd(BIT4 | BIT3);
 		}
 		write_cmd(BIT1); // return home
 		action = 0;
 		break;
 	}
 	write_cmd(BIT1); // return home
-	P1OUT |= BIT3;
-	P1IFG = 0;
+	P1OUT |= BIT3; // make sure that we're still driving P1.3 high
+	P1IFG = 0;     // clear any P1 pin interrupt flags that might have been set
 }
